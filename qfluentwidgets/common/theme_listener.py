e4121d03d6dcc621f229ee88e352ebe48e0ba0cd
Qiao <SugarQiao@outlook.com>
2025-01-12 10:27:25 +0800

    🟢refactor(Core): 优化系统主题监听器  - 添加相关注释

--- qfluentwidgets/common/theme_listener.py
+++ qfluentwidgets/common/theme_listener.py
@@ -7,22 +7,28 @@ from .config import Theme, qconfig
 
 
 class SystemThemeListener(QThread):
-    """System theme listener"""
+    """系统主题监听器"""
 
     systemThemeChanged = Signal()
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None) -> None:
         super().__init__(parent=parent)
 
-    def run(self):
+    def run(self) -> None:
+        # 使用 darkdetect 模块监听系统主题变化, 并连接到槽函数
         darkdetect.listener(self._onThemeChanged)
 
-    def _onThemeChanged(self, theme: str):
+    def _onThemeChanged(self, theme: str) -> None:
+        """主题变化时的槽函数"""
+
+        # 根据传入的 theme 参数判断系统主题
         theme = Theme.DARK if theme.lower() == "dark" else Theme.LIGHT
 
+        # 如果当前主题模式为自动, 则根据系统主题自动切换主题
         if qconfig.themeMode.value != Theme.AUTO or theme == qconfig.theme:
             return
 
+        # 切换主题
         qconfig.theme = Theme.AUTO
         qconfig._cfg.themeChanged.emit(Theme.AUTO)
         self.systemThemeChanged.emit()
