2990e564ac33b63ff5b55ab3a9a2fa3384270afb
Qiao <SugarQiao@outlook.com>
2025-01-14 10:27:39 +0800

    ♻️ refactor(Core): stacked_widget - 优化注释/类型注解

--- qfluentwidgets/components/widgets/stacked_widget.py
+++ qfluentwidgets/components/widgets/stacked_widget.py
@@ -1,7 +1,10 @@
 # coding:utf-8
 
+# 标准库导入
+from typing import List
+
 # 第三方库导入
-from PySide6.QtCore import QPoint, Signal, QEasingCurve, QAbstractAnimation, QPropertyAnimation
+from PySide6.QtCore import QPoint, Signal, QEasingCurve, QAbstractAnimation, QPropertyAnimation, QParallelAnimationGroup
 from PySide6.QtWidgets import QWidget, QStackedWidget, QGraphicsOpacityEffect
 
 
@@ -53,9 +56,25 @@ class OpacityAniStackedWidget(QStackedWidget):
 
 
 class PopUpAniInfo:
-    """Pop up ani info"""
+    """
+    弹出动画信息类
 
-    def __init__(self, widget: QWidget, deltaX: int, deltaY, ani: QPropertyAnimation):
+    Parameters
+    ----------
+    widget : QWidget
+        需要执行动画的部件
+
+    deltaX : int
+        动画过程中 x 轴方向的位移量
+
+    deltaY : int
+        动画过程中 y 轴方向的位移量
+
+    ani : QPropertyAnimation
+        控制部件位置的动画对象
+    """
+
+    def __init__(self, widget: QWidget, deltaX: int, deltaY: int, ani: QPropertyAnimation) -> None:
         self.widget = widget
         self.deltaX = deltaX
         self.deltaY = deltaY
@@ -63,33 +82,60 @@ class PopUpAniInfo:
 
 
 class PopUpAniStackedWidget(QStackedWidget):
-    """Stacked widget with pop up animation"""
+    """
+    带有弹出动画效果的堆叠窗口部件
+
+    Attributes
+    ----------
+    aniFinished : Signal
+        动画完成时发出的信号
+
+    aniStart : Signal
+        动画开始时发出的信号
+
+    aniInfos : List[PopUpAniInfo]
+        存储每个子窗口动画信息的列表
+
+    _nextIndex : int
+        即将显示的窗口索引
+
+    _ani : QPropertyAnimation
+        当前正在执行的动画对象
+    """
 
     aniFinished = Signal()
     aniStart = Signal()
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.aniInfos = []  # type: List[PopUpAniInfo]
-        self._nextIndex = None
-        self._ani = None
-
-    def addWidget(self, widget, deltaX=0, deltaY=76):
-        """add widget to window
+    def __init__(self, parent=None) -> None:
+        """
+        初始化弹出动画堆叠窗口
 
         Parameters
-        -----------
-        widget:
-            widget to be added
+        ----------
+        parent : QWidget, optional
+            父窗口, 默认为 None
+        """
+        super().__init__(parent)
+        self.aniInfos: List[PopUpAniInfo] = []
+        self._nextIndex: int | None = None
+        self._ani: QPropertyAnimation | None = None
 
-        deltaX: int
-            the x-axis offset from the beginning to the end of animation
+    def addWidget(self, widget: QWidget, deltaX: int = 0, deltaY: int = 64) -> None:
+        """
+        添加一个新的子窗口, 并设置其动画属性
 
-        deltaY: int
-            the y-axis offset from the beginning to the end of animation
+        Parameters
+        ----------
+        widget : QWidget
+            要添加的窗口部件
+
+        deltaX : int, optional
+            动画过程中 x 轴方向的位移量, 默认为 0
+
+        deltaY : int, optional
+            动画过程中 y 轴方向的位移量, 默认为 64
         """
         super().addWidget(widget)
-
         self.aniInfos.append(
             PopUpAniInfo(
                 widget=widget,
@@ -99,7 +145,15 @@ class PopUpAniStackedWidget(QStackedWidget):
             )
         )
 
-    def removeWidget(self, widget: QWidget):
+    def removeWidget(self, widget: QWidget) -> None:
+        """
+        移除指定的子窗口及其对应的动画信息
+
+        Parameters
+        ----------
+        widget : QWidget
+            要移除的窗口部件
+        """
         index = self.indexOf(widget)
         if index == -1:
             return
@@ -113,26 +167,32 @@ class PopUpAniStackedWidget(QStackedWidget):
         needPopOut: bool = False,
         showNextWidgetDirectly: bool = True,
         duration: int = 250,
-        easingCurve=QEasingCurve.OutQuad,
-    ):
-        """set current window to display
+        easingCurve: QEasingCurve = QEasingCurve.Type.OutQuad,
+    ) -> None:
+        """
+        设置当前显示的窗口索引, 并执行相应的动画
 
         Parameters
         ----------
-        index: int
-            the index of widget to display
+        index : int
+            要显示的窗口索引
 
-        isNeedPopOut: bool
-            need pop up animation or not
+        needPopOut : bool, optional
+            是否需要执行弹出动画, 默认为 False
 
-        showNextWidgetDirectly: bool
-            whether to show next widget directly when animation started
+        showNextWidgetDirectly : bool, optional
+            动画过程中是否直接显示下一个窗口, 默认为 True
 
-        duration: int
-            animation duration
+        duration : int, optional
+            动画持续时间（毫秒）, 默认为 250
 
-        easingCurve: QEasingCurve
-            the interpolation mode of animation
+        easingCurve : QEasingCurve, optional
+            动画的插值模式, 默认为 QEasingCurve.OutQuad
+
+        Raises
+        ------
+        Exception
+            如果提供的索引非法
         """
         if index < 0 or index >= self.count():
             raise Exception(f"The index `{index}` is illegal")
@@ -144,13 +204,14 @@ class PopUpAniStackedWidget(QStackedWidget):
             self._ani.stop()
             self.__onAniFinished()
 
-        # get the index of widget to be displayed
+        # 保存下一个窗口索引
         self._nextIndex = index
 
-        # get animation
+        # 获取当前和下一个窗口的动画信息
         nextAniInfo = self.aniInfos[index]
         currentAniInfo = self.aniInfos[self.currentIndex()]
 
+        # 获取当前和下一个窗口
         currentWidget = self.currentWidget()
         nextWidget = nextAniInfo.widget
         ani = currentAniInfo.ani if needPopOut else nextAniInfo.ani
@@ -167,49 +228,77 @@ class PopUpAniStackedWidget(QStackedWidget):
             self.__setAnimation(ani, pos, QPoint(nextWidget.x(), 0), duration, easingCurve)
             super().setCurrentIndex(index)
 
-        # start animation
         ani.finished.connect(self.__onAniFinished)
         ani.start()
         self.aniStart.emit()
 
     def setCurrentWidget(
         self,
-        widget,
+        widget: QWidget,
         needPopOut: bool = False,
         showNextWidgetDirectly: bool = True,
         duration: int = 250,
-        easingCurve=QEasingCurve.OutQuad,
-    ):
-        """set currect widget
+        easingCurve: QEasingCurve = QEasingCurve.OutQuad,
+    ) -> None:
+        """
+        设置当前显示的窗口部件, 并执行动画
 
         Parameters
         ----------
-        widget:
-            the widget to be displayed
+        widget : QWidget
+            要显示的窗口部件
 
-        isNeedPopOut: bool
-            need pop up animation or not
+        needPopOut : bool, optional
+            是否需要执行弹出动画, 默认为 False
 
-        showNextWidgetDirectly: bool
-            whether to show next widget directly when animation started
+        showNextWidgetDirectly : bool, optional
+            动画过程中是否直接显示下一个窗口, 默认为 True
 
-        duration: int
-            animation duration
+        duration : int, optional
+            动画持续时间（毫秒）, 默认为 250
 
-        easingCurve: QEasingCurve
-            the interpolation mode of animation
+        easingCurve : QEasingCurve, optional
+            动画的插值模式, 默认为 QEasingCurve.OutQuad
         """
         self.setCurrentIndex(self.indexOf(widget), needPopOut, showNextWidgetDirectly, duration, easingCurve)
 
-    def __setAnimation(self, ani, startValue, endValue, duration, easingCurve=QEasingCurve.Linear):
-        """set the config of animation"""
+    def __setAnimation(
+        self,
+        ani: QPropertyAnimation,
+        startValue: QPoint,
+        endValue: QPoint,
+        duration: int,
+        easingCurve: QEasingCurve = QEasingCurve.Linear,
+    ) -> None:
+        """
+        配置动画属性
+
+        Parameters
+        ----------
+        ani : QPropertyAnimation
+            动画对象
+
+        startValue : QPoint
+            动画起始位置
+
+        endValue : QPoint
+            动画结束位置
+
+        duration : int
+            动画持续时间（毫秒）
+
+        easingCurve : QEasingCurve, optional
+            动画的插值模式, 默认为 QEasingCurve.Linear
+        """
         ani.setEasingCurve(easingCurve)
         ani.setStartValue(startValue)
         ani.setEndValue(endValue)
         ani.setDuration(duration)
 
-    def __onAniFinished(self):
-        """animation finished slot"""
+    def __onAniFinished(self) -> None:
+        """
+        动画完成后的槽函数, 用于更新状态
+        """
         self._ani.finished.disconnect()
         super().setCurrentIndex(self._nextIndex)
         self.aniFinished.emit()
